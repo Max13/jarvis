9d8a0c0311562be9651fa4944b213a35aa60c4ea
Adnan RIHAN <adnan@rihan.fr>
2021-02-04 01:50:42 +0100

    ProcessTelegramDocument job sending files received to printer via ssh

---
+++ app/Jobs/ProcessTelegramDocument.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Http;
+use RuntimeException;
+use Throwable;
+
+class ProcessTelegramDocument implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * The number of seconds to wait before retrying the job.
+     *
+     * @var int
+     */
+    public $backoff = 60;
+
+    /**
+     * The number of times the job may be attempted.
+     *
+     * @var int
+     */
+    public $tries = 5;
+
+    /**
+     * Payload received from Telegram
+     *
+     * @var array
+     */
+    protected $payload;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(array $payload)
+    {
+        $this->payload = $payload;
+    }
+
+    /**
+     * Download the given url file, store it in a temporary file
+     * then returns the local filename
+     *
+     * @param  string  $url
+     * @return string|false
+     */
+    public function download($url)
+    {
+        $tmpFile = tempnam(null, 'jarvis_tg');
+
+        $fdRemote = fopen($url, 'r');
+        throw_if(
+            $fdRemote === false,
+            RuntimeException::class,
+            "Couldn’t open remote url: $url"
+        );
+
+        $fdLocal = fopen($tmpFile, 'w+');
+        throw_if(
+            $fdLocal === false,
+            RuntimeException::class,
+            "Couldn’t open temporary file: $tmpFile"
+        );
+
+        while ($buffer = fread($fdRemote, 4096)) {
+            fwrite($fdLocal, $buffer);
+        }
+
+        fclose($fdLocal);
+        fclose($fdRemote);
+
+        return $tmpFile;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $res = Http::get(
+            config('telegram.endpoint').'/bot'.config('telegram.token').'/getfile',
+            [
+                'file_id' => $this->payload['message']['document']['file_id'],
+            ]
+        );
+
+        $fileUrl = config('telegram.endpoint').'/file/bot'.config('telegram.token').'/'.$res['result']['file_path'];
+
+        $sshFile = base_path('lpuser@kitt_rsa');
+        $tmpFile = $this->download($fileUrl);
+
+        exec(
+            "ssh -i $sshFile -o 'UserKnownHostsFile /dev/null' -o 'StrictHostKeyChecking no' lpuser@kitt.home.rihan.fr lp < $tmpFile",
+            $output,
+            $result_code
+        );
+
+        throw_if(
+            $result_code !== 0,
+            RuntimeException::class,
+            implode(PHP_EOL, $output)
+        );
+
+        if ($result_code === 0) {
+            Http::post(
+                config('telegram.endpoint').'/bot'.config('telegram.token').'/sendmessage',
+                [
+                    'chat_id' => $this->payload['message']['chat']['id'],
+                    'text' => 'Sent to the printer',
+                    'reply_to_message_id' => $this->payload['message']['message_id'],
+                    'allow_sending_without_reply' => true,
+                ]
+            );
+        }
+    }
+
+    /**
+     * Handle a job failure.
+     *
+     * @param  \Throwable  $exception
+     * @return void
+     */
+    public function failed(Throwable $exception)
+    {
+        Http::post(
+            config('telegram.endpoint').'/bot'.config('telegram.token').'/sendmessage',
+            [
+                'chat_id' => $this->payload['message']['chat']['id'],
+                'text' => $exception->getMessage(),
+                'reply_to_message_id' => $this->payload['message']['message_id'],
+                'allow_sending_without_reply' => true,
+            ]
+        );
+    }
+}
